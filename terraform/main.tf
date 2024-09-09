3386a3fed9515c77cc4c6e3c14e9e831f3e15110
Michael Kapusta <michael.kapusta@ingka.com>
2024-09-09 13:16:00 +0200

    terraform init

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,31 @@
-# Terraform Configurations
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_ssm_parameter" "project_name" {
+  name = "project_name"
+  type = "String"
+  value = "poc-backstage"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-04f76ebf53292ef4d"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-08ecc36cd37a82e6b"]
+  subnet_id              = "subnet-03bad56679c8d253c"
+
+  tags = {
+    Name = "BackstageServerInstance"
+  }
+}
